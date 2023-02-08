dc8edd4552a981bfb93e0bb506d80a0581b40a28
Yaroslav Halchenko <debian@onerussian.com>
2023-02-07 20:46:16 -0500

    added command I used to populate all the clones

--- code/populate.sh
+++ code/populate.sh
@@ -3,4 +3,8 @@
 
 cd $(dirname $0)/..
 
-datalad run -m "Fetch fresh list of apps" --explicit --output apps.json bash -c "curl --silent https://brainlife.io/api/warehouse/app/query | jq '.' >| apps.json"
+# does not work on smaug for some reason: https://github.com/brainlife/brainlife/issues/220
+# datalad run -m "Fetch fresh list of apps" --explicit --output apps.json bash -c "curl --silent https://brainlife.io/api/warehouse/app/query | jq '.' >| apps.json"
+
+jq .[].github apps.json | sed -e 's,",,g' | sort | uniq | while read r; do d=apps/$r; [ -e $d ] || datalad install -d . --source https://github.com/$r $d; done
+
